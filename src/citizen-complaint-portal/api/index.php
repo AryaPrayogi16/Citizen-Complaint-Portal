e1f916a3c46ce319a0248f963830b7f9493cb369
aryaprayogi16 <aryaprayogi16@gmail.com>
2025-03-10 23:01:01 +0700

    commit

--- src/citizen-complaint-portal/api/index.php
+++ src/citizen-complaint-portal/api/index.php
@@ -1,1 +1,16 @@
-<?php require __DIR__ . '/../public/index.php';
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Check maintenance mode
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Load Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request
+(require __DIR__ . '/../bootstrap/app.php')->handleRequest(Request::capture());
